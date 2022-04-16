8171b03f7db17dcf5b6f22f6b99f690d157031ab
Kanchan <88272489+Kanchan-Bartwal@users.noreply.github.com>
2022-04-17 00:24:01 +0530

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -5,25 +5,28 @@ void bubbleSort(int arr[])
 {
   int comp = 0, swap = 0;
 	
-	for(int i=0; i<n-1; i++){
-		for(int j=0; j<n-1-i; j++){
+	for(int i=0; i<n-1; i++)
+	{
+		for(int j=0; j<n-1-i; j++)
+		{
 			comp++;
-			if(arr[j] > arr[j+1]){
-					swap++;
-					int temp = arr[j];
-					arr[j] = arr[j+1];
-					arr[j+1] = temp;
+			if(arr[j] > arr[j+1])
+			{
+				swap++;
+				int temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
 			}
 		}
 	if(swap == 0)
-			break;
+	break;
 	}
 	
 	cout<<"Sorted array: ";
 	for(int i=0; i<n; i++)
 		cout<<arr[i]<<" ";
   
-  cout<<endl;
+          cout<<endl;
 	
 	cout<<"Comparision : "<<comp<<endl;
 	cout<<"Swapping : "<<swap<<endl;
