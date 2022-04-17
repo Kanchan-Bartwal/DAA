48b84f0f06e5cfe2e78c30b952e7e265a2117b4d
Kanchan <88272489+Kanchan-Bartwal@users.noreply.github.com>
2022-04-17 12:12:06 +0530

    Update 4. Insertion Sort.cpp

--- 4. Insertion Sort.cpp
+++ 4. Insertion Sort.cpp
@@ -1,1 +1,39 @@
+#include<iostream>
+using namespace std;
 
+void insertionSort(int arr[], int n)
+{
+    for(int i=1; i<n; i++)
+    {
+        int key = arr[i];
+        int j = i-1;
+
+        while((j>=0) && (arr[j]>key))
+        {
+            arr[j+1] = arr[j];
+            j--;
+        }
+
+        arr[j+1] = key;
+    }
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter size: ";
+    cin>>n;
+
+    int arr[n];
+    cout<<"Enter array elements: ";
+    for(int i=0; i<n; i++)
+        cin>>arr[i];
+
+    insertionSort(arr,n);
+
+    cout<<"Sorted list: ";
+    for(int i=0; i<n; i++)
+        cout<<arr[i]<<" ";
+
+    return 0;
+}
