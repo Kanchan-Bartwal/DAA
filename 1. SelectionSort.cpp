0b6ebf589ad29355c94a983e42d48891520778a2
Kanchan <88272489+Kanchan-Bartwal@users.noreply.github.com>
2022-04-17 12:23:11 +0530

    Update 1. SelectionSort.cpp

--- 1. SelectionSort.cpp
+++ 1. SelectionSort.cpp
@@ -1,1 +1,41 @@
+#include<iostream>
+using namespace std;
 
+void selectionSort(int arr[], n)
+{
+  int min;
+  for(int i=0;i<n-1;i++)
+  {
+    min=i;
+    for(int j=i+1;j<n;j++)
+    {
+      if(arr[j]<arr[min])
+        min=j;
+    }
+    if(min!=i)
+    {
+      int temp=arr[i];
+      arr[i]=arr[min];
+      arr[min]=temp;
+    }
+  }
+  
+  for(int i=0;i<n;i++)
+    cout<<arr[i]<<" ";
+}
+
+int main()
+{
+  int n;
+  cout<<"Enter the number of elements"<<endl;
+  cin>>n;
+  
+  int arr[n];
+  cout<<"Enter the elements"<<endl;
+  
+  for(int i=0;i<n;i++)
+  cin>>arr[i];
+  
+  selectionSort(arr,n);
+  return 0;
+}
